31448e81c2ace6405ca264f52f1147e3278893c5
zuck007 <pradeepchoudhary009@gmail.com>
2018-02-14 18:21:06 +0530

    updated version

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function getTweetProps (tweet) {
     id = tweet.retweeted_status.id_str
   }
   tweetLink += user + '/status/' + id
-  return {'tweet_url': tweetLink, 'article_url': tweet.entities.urls[0].expanded_url}
+  return {'tweet_url': tweetLink, 'tweet_id':id, 'article_url': tweet.entities.urls[0].expanded_url}
 }
 var tweep = (username, limit) => {
   limit = limit < 5 ? 5 : limit
